ea634cb773d5f790da9d152ef35455945c608eb6
OleksiiProkoshin <my.mail2210@gmail.com>
2023-06-14 13:13:08 +0300

    +3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,12 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const gallery = document.querySelector('.gallery');
+
+const galleryHTML = images.map(
+  (image) =>
+    `<li class="gallery-item"><img src="${image.url}" alt="${image.alt}"></li>`
+);
+
+gallery.insertAdjacentHTML('beforeend', galleryHTML.join(''));
