2216e0dee9367c16dc8395b461b6e77728f2268c
OleksiiProkoshin <my.mail2210@gmail.com>
2023-06-19 17:46:11 +0300

    fix #1

--- js/task-01.js
+++ js/task-01.js
@@ -5,8 +5,8 @@ const categoryCount = categoryItems.length;
 console.log(`Number of categories: ${categoryCount}`);
 
 categoryItems.forEach((item) => {
-  const categoryTitle = item.querySelector('h2').textContent;
-  const categoryElements = item.querySelectorAll('li').length;
+  const categoryTitle = item.children[0].textContent;
+  const categoryElements = item.children[1].children.length;
   console.log(`Category: ${categoryTitle}`);
   console.log(`Elements: ${categoryElements}`);
 });
